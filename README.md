d92fe8e8d6e511613491b99a88e5c1d01b19f2bd
Sebastián Monía <smonia@outlook.com>
2021-08-20 23:24:13 -0600

    Fetch server list and display it

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 
 Small tool to lookup NordVPN servers and map them to a location. This could be a spreadsheet that I update every few months, but where's the fun in that!
 
-Depends on [nodgui](https://www.autistici.org/interzona/nodgui.html), [dexador](https://github.com/fukamachi/dexador), [jonathan](https://github.com/Rudolph-Miller/jonathan), the NordVPN (servers API)[https://api.nordvpn.com/server] , and ArcGIS [REST API](https://developers.arcgis.com/rest/geocode/api-reference/geocoding-reverse-geocode.htm).
+Depends on [nodgui](https://www.autistici.org/interzona/nodgui.html), [dexador](https://github.com/fukamachi/dexador), [jonathan](https://github.com/Rudolph-Miller/jonathan), the NordVPN [servers API](https://api.nordvpn.com/server) , and ArcGIS [REST API](https://developers.arcgis.com/rest/geocode/api-reference/geocoding-reverse-geocode.htm).
+
+
+## Installation
+
+You can compile your own version with the included `build.sh` script. You need SBCL with Quicklisp, and also Tcl and Tk.
+
+Included in the "binary" directory is a pre-packaged Tcl+Tk downloaded from http://kitcreator.rkeene.org/kitcreator (the executable renamed from `tclkit` to `wish`) and `nordlocations` that should work on Linux x64. You can drop both files to `~/.local/bin`.
+
+## Usage
+
+Execute "nordlocations", it should display a list of servers. Clicking a server name will show its location info.
 
 
 
--- nordlocations.lisp
+++ nordlocations.lisp
@@ -2,24 +2,23 @@
 
 (in-package #:nordlocations)
 
-(defvar *selected-toolbox* "" "The name of the selected the toolbox container name.")
-(defvar *command-text* "emacs" "The command to execute.")
-(defvar *available-containers* nil "List of containers available.")
+(defvar *nord-servers-url* "https://api.nordvpn.com/server" "URL to retrieve the list of VPN servers.")
+(defvar *reverse-geocode-url* "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?location=~a,~a&f=json" "URL template to replace the lat/long values and retrieve the location data.")
+(defvar *servers-data* nil "List with server information, once it is retrieved from the location specified in `*nord-servers-url*'.")
 
-(defvar *help-text* "Usage: toolbox-launcher [container-name] [command -and -arguments]
+;; Controls bound to global variables because they are accessed from multiple functions.
+(defvar *servers-listbox* nil "Holds a reference to the listbox that has the server list.")
+(defvar *location-label* nil "Holds a reference to the label that needs to be updated with location data.")
 
-Call \"toolbox run\" with the parameters specified in the UI.
-The optional command line arguments allow pre-filling the values of the window.")
+(defvar *help-text* "Execute \"nordlocations\" to display a window to retrieve the list of NordVPN servers. When you select a server name, on the side you will see its location info.")
 
 (defun init ()
-  "Initialize the application based on the CLI arguments (if any) and start the UI."
+  "Start the UI, show the help text if needed"
   (let ((arguments (uiop:command-line-arguments)))
-    (if (string= "-h" (first arguments))
-        (show-help-and-exit)
-        (progn
-          (setf *selected-toolbox* (first arguments))
-          (setf *command-text* (format nil "~{~a ~}" (or (rest arguments) '("emacs"))))
-          (get-available-toolboxes))))
+    (when (string= "-h" (first arguments))
+      (show-help-and-exit)))
+  ;; This would be a good place to set *nord-servers-url* and
+  ;; *reverse-geocode-url* from a config file. Or something.
   (start-ui))
 
 (defun show-help-and-exit ()
@@ -27,49 +26,61 @@ The optional command line arguments allow pre-filling the values of the window."
   (uiop:quit 0))
 
 (defun start-ui ()
-  "Launches the UI for toolbox-launcher."
-  (with-nodgui (:title "toolbox launcher")
+  "Launches the UI for nordlocations."
+  (with-nodgui (:title "NordVPN - Server Locations")
     (font-configure "TkDefaultFont" :size 12)
     (font-configure "TkTextFont" :size 12)
-    (let* ((toolbox-label (make-instance 'label
-                                         :text "Toolbox:"))
-           (toolbox-combo (make-instance 'combobox
-                                         ;; hardcoded...
-                                         :text *selected-toolbox*
-                                         :values *available-containers*))
-           (command-label (make-instance 'label
-                                         :text "Command:"))
-           (command-entry (make-instance 'entry
-                                         :text *command-text*))
-           (exec-button (make-instance 'button
-                                       :text "Execute"
-                                       :command (lambda ()
-                                                  (setf *selected-toolbox* (text toolbox-combo)
-                                                        *command-text* (text command-entry))
-                                                  (run-program-and-exit)))))
+    (let* ((server-label (make-instance 'label
+                                        :text "Server:"))
+           (server-list (make-instance 'scrolled-listbox))
+           (location-title-label (make-instance 'label
+                                          :text "Location info:"))
+           (location-info-label (make-instance 'label
+                                               :text (make-string 40 :initial-element #\Space)))
+           (fetch-servers-button (make-instance 'button
+                                                :text "Get server list"
+                                                :command #'populate-server-list-start)))
+      (setf *servers-listbox* server-list)
+      (setf *location-label* location-info-label)
       ;; focus on the combo to select the container
-      (focus toolbox-combo)
-      ;; Make exec-button the default action
+      (focus fetch-servers-button)
+      ;; Make fetch-servers-button the default action...need testing to determine
+      ;; if this is convenient or annoying
       (bind *tk* "<Return>" (lambda (evt)
                               (declare (ignore evt))
-                              (funcall (command exec-button))))
-      (grid toolbox-label 0 0 :padx 10 :pady 10 :sticky "w")
-      (grid toolbox-combo 0 1 :padx 10 :pady 10 :sticky "w")
-      (grid command-label 1 0 :padx 10 :pady 10 :sticky "w")
-      (grid command-entry 1 1 :padx 10 :pady 10 :sticky "w")
-      (grid exec-button 2 0 :padx 10 :pady 10 :columnspan 2)
+                              (funcall (command fetch-servers-button))))
+      ;; when the listbox selection changes, we have to fetch the data
+      (bind (listbox server-list) "<<ListboxSelect>>" #'handle-server-selected)
+
+      (grid server-label 0 0 :padx 10 :pady 10 :sticky "w")
+      (grid fetch-servers-button 0 1 :padx 10 :pady 10 :sticky "w")
+      (grid server-list 1 0 :padx 10 :pady 10 :sticky "w" :columnspan 2)
+      (grid location-title-label 0 2 :padx 10 :pady 10 :sticky "w")
+      (grid location-info-label 1 2 :padx 10 :pady 10 :sticky "w")
       (grid-columnconfigure *tk* :all :weight 1)
       (grid-rowconfigure    *tk* :all :weight 1))))
 
-(defun get-available-toolboxes ()
-  "Call toolbox to list the available containers, and parse its output."
-  (let* ((list-output (uiop:run-program '("toolbox" "list" "-c")
-                                        :output '(:string :stripped t)))
-         (containers (subseq (uiop:split-string list-output :separator '(#\Newline)) 1)))
-    (setf *available-containers* (loop for line in containers
-                                       collect (third (uiop:split-string line))))
-    (unless *selected-toolbox*
-      (setf *selected-toolbox* (first *available-containers*)))))
+(defun populate-server-list-start ()
+  "Fetch the list of servers from `*nord-servers-url*' and fill the server listbox."
+  (listbox-delete *servers-listbox*)
+  (listbox-append *servers-listbox* "Retrieving the servers list...")
+  ;; not so subtle attempt to delay a bit the next step...
+  (nodgui:after 100 #'populate-server-list-complete))
+
+(defun populate-server-list-complete ()
+  (let ((parsed-server-data (jonathan:parse (dex:get "https://api.nordvpn.com/server")
+                                            :as :hash-table)))
+    (setf *servers-data* (sort parsed-server-data
+                               #'string-lessp
+                               :key (lambda (server) (gethash "name" server))))
+    (listbox-delete *servers-listbox*)
+    (loop for server in *servers-data*
+          do (listbox-append *servers-listbox* (gethash "name" server)))))
+
+(defun handle-server-selected (evt)
+  (let ((item-index (first (first (listbox-get-selection *servers-listbox*)))))
+    (format t "raw: ~a index: ~a" (listbox-get-selection *servers-listbox*) item-index)
+    (format t "Get the info for ~a ~%" (gethash "domain" (nth item-index *servers-data*)))))
 
 (defun run-program-and-exit ()
   "Run the program according to the parameters in the UI and exit."
